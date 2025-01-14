23ba213783e3021c56f266a88c43d7b1298c6f79
aznshrllh <ahmadzakinashirullah@gmail.com>
2025-01-14 19:08:38 +0700

    studying graphQL add and update

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ type Book {
     author: String
   }
 
+  #! Query for GET
   type Query {
     books: [Book]
     getBook(id: ID): Book
+  }
+
+  input BookInput {
+    title: String
+    author: String
+  }
+
+  #! Mutation for POST, PUT, DELETE, PATCH
+  type Mutation {
+    addBook(newBook: BookInput): Book
+    updateBook(id:ID, newBook: BookInput): Book
   }
     `;
 
@@ -30,10 +42,29 @@ const books = [
 const resolvers = {
   Query: {
     books: () => books,
-    getBook: (parent, args) => {
+    getBook: (_, args) => {
       const { id } = args;
       const book = books.find((book) => book.id === Number(id));
       return book;
+    },
+  },
+
+  Mutation: {
+    addBook: (_, args) => {
+      const { title, author } = args;
+      const newBook = {
+        id: books.length + 1,
+        title,
+        author,
+      };
+      books.push(newBook);
+      return newBook;
+    },
+    updateBook: (_, args) => {
+      const { id, newBook } = args;
+      const bookIndex = books.findIndex((book) => book.id === Number(id));
+      books[bookIndex] = { ...books[bookIndex], ...newBook };
+      return books[bookIndex];
     },
   },
 };
